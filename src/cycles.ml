ab7f3c3611e377ca259e1e784f43ad5edef24235
Ian A. Mason <iam@csl.sri.com>
2015-05-23 11:52:57 -0700

    Torturous start to the long weekend, III.

--- src/cycles.ml
+++ src/cycles.ml
@@ -6,6 +6,12 @@ open Bc
 
 module G = Pack.Digraph
 
+(*
+ * The nodes in the digraph are integers.
+ * The index of a block is its position in fu.fblocks.
+ * This hashtbl maps the block name v to its index.
+ *
+ *)
 let create_indices fu n =
   let indices = Hashtbl.create n in
     for i=0 to n - 1 do
@@ -13,8 +19,16 @@ let create_indices fu n =
     done;
     indices
       
-	     
-	     
+(*
+ * This creates the digraph associated with the finfo.
+ * The nodes in the digraph are the indexes of the blocks 
+ * The edges in the digraph are the successors, viewed
+ * i.e. the index of the source block to the index of
+ * the successor block.
+ *
+ *)
+      
+    
 let fu_to_graph fu =
   (* the number of blocks a.k.a nodes *)
   let nodecount = (List.length fu.fblocks) in
@@ -27,5 +41,5 @@ let fu_to_graph fu =
   let add_edge = (fun v0 v1 -> (G.add_edge g (get_index v0) (get_index v1))) in
     Hashtbl.iter add_edge  edges;
     g
-
+      
       
