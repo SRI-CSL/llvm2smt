36505be97a295289aa35ccc17baa25f8c3c6a3d7
Ian A. Mason <iam@csl.sri.com>
2015-05-23 10:33:56 -0700

    ocamlfind magic.

--- src/cycles.ml
+++ src/cycles.ml
@@ -1,11 +1,13 @@
 (* graph playground *)
 
 open Graph
+open Bc
+   
 
 module G = Pack.Digraph
 
 let fu_to_graph fu =
-  let g = G.create (List.length fu.fblocks) () in
+  let g = (G.create ?size:(Some(List.length fu.fblocks)) ()) in
     g
 
       
