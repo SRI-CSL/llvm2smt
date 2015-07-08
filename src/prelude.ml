77427fbc9af1d8ad328dafc9594e837c4162fdfb
Ian A. Mason <iam@csl.sri.com>
2015-07-07 18:38:42 -0700

    Now need to learn about buffers...

--- src/prelude.ml
+++ src/prelude.ml
@@ -450,6 +450,22 @@ let vtrunc_3 b n w  =
 	n w w n  n w  n w  n w  n w  n w  n w  n w  n w  n
 	
       
+(*
+ * Ian thinking out loud here: this would spec out what we need in our prelude.
+ * It would be filled in while translating the cu.
+ * We would then use it to limit the prelude definitions to those that are
+ * necessary.
+*)
+
+type prelude = {
+  mutable vector: (int * int) list;
+  mutable vundef: (int * int) list;
+  mutable vmake:  (int * int) list;
+  mutable vzero:  (int * int) list;
+  mutable vbinop: (int * int) list;  (* could include the actual operator *)
+  mutable trunc:  (int * int) list;
+  mutable vtrunc: (int * int * int) list;
+}
       
       
       
