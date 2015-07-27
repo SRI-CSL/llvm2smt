aefd573a9e82232ea5a74cb06b10bac95db73993
Ian A. Mason <iam@csl.sri.com>
2015-07-27 11:06:26 -0700

    Hope I never see an < 8 x T > cast.

--- src/prelude.ml
+++ src/prelude.ml
@@ -406,7 +406,43 @@ let vector_bool b logv =
   else
     failwith("vector_bool Ooops: need to write some prelude code for vectors longer than 2^3: "^(string_of_int logv))
 
-    
+
+
+let v2b_cast b w =
+  let w2 = 2 * w in 
+  let w4 = 2 * w2 in 
+    bprintf b
+      "
+;; this assumes little endian representation
+(define-fun cast_vector_1_%d_to_bits ((x vector_1_%d)) (_ BitVec %d)
+   (concat (select x #b1) (select x #b0)))
+
+;; this assumes little endian representation
+(define-fun cast_vector_2_%d_to_bits ((x vector_2_%d)) (_ BitVec %d)
+   (concat (concat (concat (select x #b11) (select x #b10)) (select x #b01)) (select x #b00)))
+"
+      w w w2   w w w4 
+
+let b2v_cast b w =
+  let w2 = 2 * w in 
+  let w3 = 3 * w in 
+  let w4 = 2 * w2 in 
+    bprintf b
+      "
+(define-fun cast_bits_to_vector_1_%d ((w (_ BitVec %d))) vector_1_%d
+   (let ((z0 ((_ extract %d 0) w))
+         (z1 ((_ extract %d %d) w)))
+      (vmake_1_%d z0 z1)))
+
+(define-fun cast_bits_to_vector_2_%d ((w (_ BitVec %d))) vector_2_%d
+   (let ((z0 ((_ extract %d 0) w))
+         (z1 ((_ extract %d %d) w))
+         (z2 ((_ extract %d %d) w))
+         (z3 ((_ extract %d %d) w)))
+      (vmake_2_%d z0 z1 z2 z3)))
+  "
+      w w2 w (w - 1) (w2 - 1) w w    w w4 w (w - 1) (w2 - 1) w (w3 - 1) w2 (w4 - 1) w3 w
+      
 let vector_casts = "
 
 
